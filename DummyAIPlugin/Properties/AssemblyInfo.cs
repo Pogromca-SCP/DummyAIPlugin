4f0d5b8b5b6c60b7f60f6a4b54be9c753438f0de
Adam Szerszenowicz <70147389+Pogromca-SCP@users.noreply.github.com>
2026-03-29 17:22:15 +0200

    Added copyright year

--- DummyAIPlugin/Properties/AssemblyInfo.cs
+++ DummyAIPlugin/Properties/AssemblyInfo.cs
@@ -3,7 +3,7 @@ using System.Reflection;
 [assembly: AssemblyTitle(DummyAIPlugin.DummyAIPlugin.PluginName)]
 [assembly: AssemblyDescription(DummyAIPlugin.DummyAIPlugin.PluginDescription)]
 [assembly: AssemblyProduct(DummyAIPlugin.DummyAIPlugin.PluginName)]
-[assembly: AssemblyCopyright("Copyright © " + DummyAIPlugin.DummyAIPlugin.PluginAuthor)]
+[assembly: AssemblyCopyright("Copyright © 2026 " + DummyAIPlugin.DummyAIPlugin.PluginAuthor)]
 [assembly: AssemblyVersion(DummyAIPlugin.DummyAIPlugin.PluginVersion)]
 [assembly: AssemblyFileVersion(DummyAIPlugin.DummyAIPlugin.PluginVersion)]
 [assembly: AssemblyInformationalVersion(DummyAIPlugin.DummyAIPlugin.PluginVersion)]
